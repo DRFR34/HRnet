33aa2c6c2c6090ccc248ecaa20d253d39b200a10
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-08-03 12:15:12 +0200

    Create wealthHealthDepts.js

---
+++ front/src/assets/data/wealthHealthDepts.js
@@ -0,0 +1,9 @@
+const departments = [
+    { value: 'Sales', label: 'Sales' },
+    { value: 'Marketing', label: 'Marketing' },
+    { value: 'Engineering', label: 'Engineering' },
+    { value: 'Legal', label: 'Legal' },    
+    { value: 'Human Resources', label: 'Human Resources' }
+];
+
+export default departments;
