ae0ac7b9454b4999ae0e836734c3a8b444cec37e
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-08-06 14:08:32 +0200

    Create TodayIcon.jsx

---
+++ front/src/components/IconsComponents/TodayIcon.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function TodayIcon({ width = 24, height = 24, color = 'currentColor' }) {
+
+
+    return (
+
+        <svg
+            width={width}
+            height={height}
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={color}
+        >
+            <path d="M32,456a24,24,0,0,0,24,24H456a24,24,0,0,0,24-24V176H32Zm80-238.86a9.14,9.14,0,0,1,9.14-9.14H230.86a9.14,9.14,0,0,1,9.14,9.14V326.86a9.14,9.14,0,0,1-9.14,9.14H121.14a9.14,9.14,0,0,1-9.14-9.14Z" />
+            <path d="M456,64H400.08V32h-48V64H159.92V32h-48V64H56A23.8,23.8,0,0,0,32,87.77V144H480V87.77A23.8,23.8,0,0,0,456,64Z" />
+        </svg>
+    );
+
+}
+
+<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_2_19)">
+<path d="M0 30C0 24.4772 4.47715 20 10 20H160C165.523 20 170 24.4772 170 30V58H0V30Z" fill="#131313"/>
+<path d="M37 10C37 4.47715 41.4772 0 47 0H57C62.5228 0 67 4.47715 67 10V32H37V10Z" fill="#131313"/>
+<path d="M109 10C109 4.47715 113.477 0 119 0H129C134.523 0 139 4.47715 139 10V32H109V10Z" fill="#131313"/>
+<path d="M0 166H172V179C172 184.523 167.523 189 162 189H10C4.47716 189 0 184.523 0 179V166Z" fill="#131313"/>
+<path d="M172 122H2V139H172V122Z" fill="#131313"/>
+<path d="M172 69H0V90H172V69Z" fill="#131313"/>
+<path d="M21 76H0V179H21V76Z" fill="#131313"/>
+<path d="M72 76H51V179H72V76Z" fill="#131313"/>
+<path d="M122 76H101V179H122V76Z" fill="#131313"/>
+<path d="M172 76H151V179H172V76Z" fill="#131313"/>
+</g>
+<defs>
+<clipPath id="clip0_2_19">
+<rect width="64" height="64" fill="none"/>
+</clipPath>
+</defs>
+</svg>
+
