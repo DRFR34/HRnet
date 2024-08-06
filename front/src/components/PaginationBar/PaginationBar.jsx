7ebd41c66406e8562f28c9a23e88907a85979deb
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-08-06 20:01:44 +0200

    Update PaginationBar.jsx
    Fixing of minor bug with the pagination's refreshing

--- front/src/components/PaginationBar/PaginationBar.jsx
+++ front/src/components/PaginationBar/PaginationBar.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect } from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentPage} from '../../redux/features/employees/employeesSlice';
+
 import './PaginationBar.scss';
 
-
-export default function PaginationBar({ currentPage, setCurrentPage, totalPages, searchValue, rowsPerPage, totalEntries, handlePageChange }) {
+export default function PaginationBar({ totalPages, searchValue, totalEntries, handlePageChange }) {
+  const dispatch = useDispatch();
+  const currentPage = useSelector((state) => state.employees.currentPage);
+  const rowsPerPage = useSelector((state) => state.employees.rowsPerPage);
+  const searchIsTriggered = useSelector((state) => state.employees.searchIsTriggered);
+  const autoSearch = useSelector((state) => state.employees.autoSearch);
 
   const startEntry = (currentPage - 1) * rowsPerPage + 1;
   const endEntry = Math.min(currentPage * rowsPerPage, totalEntries);
 
-  /**  
-   * Ensures the current page is not out of bound, when changing the number of rows per page, or when changing the search value
-   * 
-  */
+  /**
+   * The two following useEffect are used to ensure the current page is not out of bound, when changing the number of rows per page, or when changing the search value, or when launching the search
+   */
   useEffect(() => {
-    setCurrentPage(1);
-  }, [rowsPerPage, searchValue]);
+    dispatch(setCurrentPage(1));
+  }, [rowsPerPage, searchValue, dispatch]);
+
+  useEffect(() => {
+    if (searchIsTriggered || autoSearch) {
+      dispatch(setCurrentPage(1));
+    }
+  }, [searchIsTriggered, autoSearch]);
 
   return (
     <div className="paginationBar">
-
       <div className="paginationBar__entriesInfos">
         Showing {startEntry} to {endEntry} of {totalEntries} entries
       </div>
@@ -33,8 +44,7 @@ export default function PaginationBar({ currentPage, setCurrentPage, totalPages,
           Previous
         </button>
 
-        <span
-          className="paginationBar__Nav__Text">
+        <span className="paginationBar__Nav__Text">
           Page {currentPage} of {totalPages}
         </span>
 
@@ -46,10 +56,7 @@ export default function PaginationBar({ currentPage, setCurrentPage, totalPages,
         >
           Next
         </button>
-
       </div>
-
     </div>
   );
 }
-
