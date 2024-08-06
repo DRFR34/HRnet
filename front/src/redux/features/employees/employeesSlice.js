595cecdb8691d96e07b82cc62bbc44aa8e73db3f
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-08-06 15:31:44 +0200

    Update employeesSlice.js
    Improve the incorporation of the components' formerly local states into the global state

--- front/src/redux/features/employees/employeesSlice.js
+++ front/src/redux/features/employees/employeesSlice.js
@@ -1,4 +1,66 @@
-// employeesSlice.js
+// // employeesSlice.js
+
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import StorageService from '../../../utils/StorageService';
+
+// export const fetchEmployees = createAsyncThunk(
+//   'employees/fetchEmployees',
+//   async () => {
+//     const response = await StorageService.getEmployees();
+//     return response;
+//   }
+// );
+
+// const employeesSlice = createSlice({
+//   name: 'employees',
+//   initialState: {
+//     employees: [],
+//     rowsPerPage: 25,
+//     selectedColumns:[
+//       'firstName',
+//       'lastName',
+//       'startDate',
+//       'department',
+//       'birthDate',
+//       'street',
+//       'city',
+//       'state',
+//       'zipCode'
+//     ],
+//     status: 'idle',
+//     error: null
+//   },
+//   reducers: {
+//     setRowsPerPage: (state, action) => {
+//       state.rowsPerPage = action.payload;
+//     },
+
+//     setSelectedColumns: (state, action) => {
+//       state.selectedColumns = action.payload;
+//     },
+
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchEmployees.pending, (state) => {
+//         state.status = 'loading';
+//       })
+//       .addCase(fetchEmployees.fulfilled, (state, action) => {
+//         state.status = 'succeeded';
+//         state.employees = action.payload;
+//       })
+//       .addCase(fetchEmployees.rejected, (state, action) => {
+//         state.status = 'failed';
+//         state.error = action.error.message;
+//       });
+//   }
+// });
+// export const { setRowsPerPage, setSelectedColumns } = employeesSlice.actions;
+// export const selectAllEmployees = (state) => state.employees.employees;
+// export const selectEmployeesStatus = (state) => state.employees.status;
+// export const selectEmployeesError = (state) => state.employees.error;
+
+// export default employeesSlice.reducer;
 
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import StorageService from '../../../utils/StorageService';
@@ -16,7 +78,7 @@ const employeesSlice = createSlice({
   initialState: {
     employees: [],
     rowsPerPage: 25,
-    selectedColumns:[
+    selectedColumns: [
       'firstName',
       'lastName',
       'startDate',
@@ -27,6 +89,11 @@ const employeesSlice = createSlice({
       'state',
       'zipCode'
     ],
+    currentPage: 1,
+    searchValue: '',
+    autoSearch: false,
+    searchIsTriggered: false,
+    filteredData: [],
     status: 'idle',
     error: null
   },
@@ -34,11 +101,24 @@ const employeesSlice = createSlice({
     setRowsPerPage: (state, action) => {
       state.rowsPerPage = action.payload;
     },
-
     setSelectedColumns: (state, action) => {
       state.selectedColumns = action.payload;
     },
-
+    setCurrentPage: (state, action) => {
+      state.currentPage = action.payload;
+    },
+    setSearchValue: (state, action) => {
+      state.searchValue = action.payload;
+    },
+    setAutoSearch: (state, action) => {
+      state.autoSearch = action.payload;
+    },
+    setSearchIsTriggered: (state, action) => {
+      state.searchIsTriggered = action.payload;
+    },
+    setFilteredData: (state, action) => {
+      state.filteredData = action.payload;
+    }
   },
   extraReducers: (builder) => {
     builder
@@ -55,7 +135,17 @@ const employeesSlice = createSlice({
       });
   }
 });
-export const { setRowsPerPage, setSelectedColumns } = employeesSlice.actions;
+
+export const { 
+  setRowsPerPage, 
+  setSelectedColumns,
+  setCurrentPage,
+  setSearchValue,
+  setAutoSearch,
+  setSearchIsTriggered,
+  setFilteredData 
+} = employeesSlice.actions;
+
 export const selectAllEmployees = (state) => state.employees.employees;
 export const selectEmployeesStatus = (state) => state.employees.status;
 export const selectEmployeesError = (state) => state.employees.error;
